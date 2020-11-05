6d58121f5d80eb592aafc852089ea0aaa51ec158
Sayan Chakroborty <sayan@disroot.org>
2020-11-05 22:54:26 +0530

    Update Fedora-Mirrors-Setup.sh

--- Fedora-Mirrors-Setup.sh
+++ Fedora-Mirrors-Setup.sh
@@ -6,6 +6,10 @@ sudo sed -i 's/metalink/#metalink/g' /etc/yum.repos.d/*
 
 sudo sed -i 's/#metalink/metalink/g' /etc/yum.repos.d/fedora-cisco*
 
+sudo dnf check-update -y
+
+dnf check-update -y
+
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 sudo sed -i 's/#baseurl/baseurl/g' /etc/yum.repos.d/rpmfusion*
